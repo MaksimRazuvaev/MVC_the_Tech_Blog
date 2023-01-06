d59f65a1c58e3b5f418b3c292fb5ca0279eb34e3
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-01-05 19:40:30 -0800

    files from models folder are finished. DB tables js files Modelsa are done

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,40 @@
 // to create models and sequilize them
+
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id'
+        },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
